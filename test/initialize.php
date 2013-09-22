5e7698788ffcbcc261177b70e78fe97a60cc35bf
methyleneprince <methyleneprince@gmail.com>
2013-09-22 16:54:51 +0800

    autocommit

--- test/initialize.php
+++ test/initialize.php
@@ -11,81 +11,102 @@
 	mysql_select_db($dbname) or die(mysql_error());
 	
     //Drop tables
-    mysql_query("DROP TABLE IF EXISTS Categorization");
-    mysql_query("DROP TABLE IF EXISTS SubCategory");
-    mysql_query("DROP TABLE IF EXISTS MetaCategory");
-    mysql_query("DROP TABLE IF EXISTS Status");
-    mysql_query("DROP TABLE IF EXISTS History");
+    mysql_query("
+        DROP TABLE IF EXISTS Categorization;
+        DROP TABLE IF EXISTS SubCategory;
+        DROP TABLE IF EXISTS MetaCategory;
+        DROP TABLE IF EXISTS Status;
+        DROP TABLE IF EXISTS History
+    ");
     
 	//Status
 	mysql_query("
 		CREATE TABLE Status (
 			ID INT AUTO_INCREMENT PRIMARY KEY,
-			Name VARCHAR(50) NOT NULL,
+			Name VARCHAR(50) UNIQUE NOT NULL,
 			Description VARCHAR(255)
-		)
-	");
-	mysql_query("
-		INSERT INTO statuses (Name) 
-		VALUES 
+		);
+        
+		INSERT INTO statuses (Name) VALUES 
 			('Pending'),
 			('Processed'),
-			('Void')
+			('Void');
+	");
+    
+    //History
+	mysql_query("
+		CREATE TABLE History (
+            TransactionID INT NOT NULL AUTO_INCREMENT,
+            Description VARCHAR(255) NOT NULL,
+            Comment VARCHAR(65535),
+            RecordedDate DATETIME DEFAULT(GETDATETIME()),
+            TransactionDate DATETIME,
+            PaymentDate DATETIME,
+            RecordedPersonID INT NOT NULL,
+            ResponsibleParty VARCHAR(255),
+            AssociatedParty VARCHAR (255),
+            StatusID INT NOT NULL,
+            Amount INT NOT NULL
+            
+            PRIMARY KEY HistoryID (TransactionID, RecordedDate),
+            
+            FOREIGN KEY (StatusID) 
+                REFERENCES Status(ID)
+                ON DELETE CASCADE
+		);
+        
+        INSERT INTO History (TransactionID, Description, AssociatedParty, Amount, StatusID) 
+        VALUES
+            (
+                SELECT 1, 'Jimmy Neutron’s membership', 'Jimmy Neutron', 1000, StatusID
+                FROM Status WHERE Name = 'Pending'
+            ),
+            (
+                SELECT 2, 'Jombles Notronbo’s family membership', 'Jombles Notronbo', 4000, StatusID 
+                FROM Status WHERE Name = 'Pending'
+            ),
+            (
+                SELECT 2, 'Jombles Notronbo’s family membership', 'Jombles Notronbo', 4000, StatusID 
+                FROM Status WHERE Name = 'Processed'
+            );   
 	");
 	
 	//Category and SubCategory
 	mysql_query("
 		CREATE TABLE Category (
-			ID INT AUTO_INCREMENT,
-            Name VARCHAR(50) NOT NULL,
-			Description VARCHAR(255),
-            PRIMARY KEY (ID)
-		)
-	");
-	mysql_query("
+			ID INT AUTO_INCREMENT PRIMARY KEY,
+            Name VARCHAR(50) UNIQUE NOT NULL,
+			Description VARCHAR(255)
+		);
+        
 		CREATE TABLE SubCategory (
 			ID INT AUTO_INCREMENT PRIMARY KEY,
             CategoryID INT NOT NULL,
-            Name VARCHAR(50) NOT NULL,
+            Name VARCHAR(50),
 			Description VARCHAR(255),
-            INDEX category_ind (CategoryID),
+            
             FOREIGN KEY (CategoryID)
                 REFERENCES Category(ID)
                 ON DELETE CASCADE
-                
-		)
-	");
+		);
         
-	
-	//History
-	mysql_query("DROP TABLE IF EXISTS History");
-	mysql_query("
-		CREATE TABLE History (
-			ID INT AUTO_INCREMENT PRIMARY KEY,
-		)
-	");
-	
-		
-mysql_query($con,"INSERT INTO History (TransactionID, Description, Comment, Associated Party, StatusID, RecordDate, Value) 
-VALUES (1, “Jimmy Neutron’s membership”, “paid in meatballs”, “Jimmy Neutron”, 2, “5/9/2013”, 10)");
-mysql_query($con,"INSERT INTO History (TransactionID, Description, Comment, Associated Party, StatusID, RecordDate, Value) 
-VALUES (1, “Jombles Notronbo’s family membership”, “”, “Jombles Notronbo”,1, “5/9/2013”, 40)");
-mysql_query($con,"INSERT INTO History (TransactionID, Description, Comment, Associated Party, StatusID, RecordDate, Value) 
-VALUES (1, “Jombles Notronbo’s family membership”, “”, “Jombles Notronbo”,2, “6/9/2013”, 40)");
-mysql_query($con,"INSERT INTO Categories (CategoryID, MetaCategory, SubCategory) 
-VALUES (1, “Membership”, “Single”)");
-mysql_query($con,"INSERT INTO Categories (CategoryID, MetaCategory, SubCategory) 
-VALUES (2, “Membership”, “Family”)");
-mysql_query($con,"INSERT INTO Categories (CategoryID, MetaCategory, SubCategory) 
-VALUES (3, “Event”, “Oktoberfest”)");
-mysql_query($con,"INSERT INTO Categories (CategoryID, MetaCategory, SubCategory) 
-VALUES (4, “Event”, “Midsommar”)");
-mysql_query($con,"INSERT INTO Categories (CategoryID, MetaCategory, SubCategory) 
-VALUES (5, “Expense”, “Admin”)");
-mysql_query($con,"INSERT INTO Categories (CategoryID, MetaCategory, SubCategory) 
-VALUES (6, “Year”, “2013”)");
-mysql_query($con,"INSERT INTO Categorization (CategorizationID, TransactionID, CategoryID) 
-VALUES (1, 1, 1)");
-mysql_query($con,"INSERT INTO Categorization (CategorizationID, TransactionID, CategoryID) 
-VALUES (2, 2, 2)");
+        INSERT INTO Category (Name) VALUES
+            ('Membership'),
+            ('Event'),
+            ('Expense'),
+            ('Year');
+            
+        INSERT INTO Subcategory (Name, CategoryID) VALUES
+            SELECT 'Single', ID FROM Category WHERE Name = 'Membership',
+            SELECT 'Family', ID FROM Category WHERE Name = 'Membership',
+            SELECT 'Oktoberfest', ID FROM Category WHERE Name = 'Event',
+            SELECT 'Midsommar', ID FROM Category WHERE Name = 'Event',
+            SELECT 'Admin', ID FROM Category WHERE Name = 'Expense',
+            SELECT '2013', ID FROM Category WHERE Name = 'Year';
+    ");    
+
+    //Categorization
+    mysql_query("
+        CREATE TABLE Categorization (
+            
 ?>
