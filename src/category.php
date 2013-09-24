aa3d8045721718b53482a5cc2c0b70d45cf2d135
tiagogarcia <21260915@student.uwa.edu.au>
2013-09-24 19:21:28 +0800

    Category Page edition

--- src/category.php
+++ src/category.php
@@ -10,14 +10,44 @@
 		</style>
 
 		<script>
+		// Validation functions ------ start
+		function validateForm(form)
+		{
+			var error = isEmpty(form.catName) 
+				
+			if(error != "")
+			{
+				alert("Some fields need correction: \n" + error);
+				return false;
+			}
+			return true;
+		}
+		
+		function isEmpty(field)
+		{
+			var error = "";
+			
+			var value = field.value.trim();
+			if(value == "" || value.length==0)
+			{
+				error = "Please enter a value in '" + field.name + "'\n";
+				field.style.background = '#E6CCCC';
+			}
+			else
+			{	
+				field.style.background = 'White';
+			}
+			return error;
+		}
+		
 		function saveAction( )
 		{
-			// to do: save action
+			alert("to do: saveAction");
 		}
 		
 		function editAction( )
 		{
-			// to do: edit action
+			alert("to do:");
 		}
 		
 		</script>
@@ -37,19 +67,21 @@
 						// Connet to database
 						$connection =mysql_connect("localhost","root","") or die("Could not connect");	
 						mysql_select_db("transaction") or die("Unable to select database!!");
-												
+						
+						// Defining variables
+						$catName = "";						
 					?>	
 
 					<table class = "formatted">
 						<!-- action="toMe.php" -->
 
-						<form name="categoryForm"  action="toMe.php" method="post">
+						<form name="categoryForm"  method="get">
 							<tr>
 								<td class = "categoryName">
 									Name*:
 								</td>
 								<td>
-									<input type="text" class="data" name="Date_trans" size="50" value="">
+									<input type="text" class="data" name="catName" size="50" value=<?php echo $catName?>>
 								</td>
 							</tr>
 							<tr>
@@ -57,13 +89,13 @@
 									Description:
 								</td>
 								<td>
-									<input type="text" class="data" name="Amount" size="50">
+									<input type="text" class="data" name="desc" size="50">
 								</td>
 							</tr><tr>* This field is compulsory</tr>
 					</table>
 					<br>					
 						</form>
-							<button onclick="saveAction()">Save Category</button>
+							<button input type="submit" onclick="saveAction()">Save Category</button>
 							<button onclick="deleteAction()">Delete Category</button>
 				</div>
 				<!-- end content!-->
@@ -71,8 +103,10 @@
 			</div><!-- end box -->
 			
 			<div id="sidebar">
-            <?php include_once("sidebar.php");?>
-            </div>
+			
+				<!-- to do: load sidebar -->
+
+			</div><!-- end sidebar -->
 			
 		</div><!-- end main -->   
 
