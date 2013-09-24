ace794d27fad8b4fb3ff5a18a60452d0d798e8f3
ktp123 <ktp123@users.noreply.github.com>
2013-09-25 00:09:05 +0800

    Update transactionDraft.php

--- src/transactionDraft.php
+++ src/transactionDraft.php
@@ -1,25 +1,31 @@
 <!DOCTYPE html>
+<?php
+$connection =mysql_connect("localhost","test","test") or die("Could not connect");
+
+mysql_select_db("test") or die("Unable to select database");
+?>
 
 <html>
-	<head>
-		<title>TAB TITLE</title>
-		
-		<style type="text/css" media="screen">
-			@import url("style2.css");
-			@import url("styling.css");
-		</style>
+<head>
+	<title>TAB TITLE</title>
+    
+    <style type="text/css" media="screen">
+        @import url("style2.css");
+		@import url("styling.css");
+    </style>
 
-		<script>
+<script>
 
 			// Validation functions ------ start
 			function validateForm(form)
 			{
 				var error = "";
-				error  += isEmpty(form.TransactionName) 
-				+ isEmpty(form.Description)
-				+ isEmpty(form.Date_trans)
-				+ isEmpty(form.Date_payRec)
-				+ isEmpty(form.Resp)
+				error  += isEmpty(form.Description) 
+				+ isEmpty(form.ResponsibleParty)
+				+ isEmpty(form.AssociatedParty)
+				+ isEmpty(form.PaymentDate)
+				+ isEmpty(form.TransactionDate)
+				+ isEmpty(form.Comment)
 				+ validateInt(form.Amount)
 				+ validateRadio("Type");
 					
@@ -102,16 +108,36 @@
 					if(regex.test(elements[i].className))
 					{
 						if(bool)
+						{
 							elements[i].setAttribute("readonly","readonly");
+						//	elements[i].reset();	// TODO : doesnt work, fixies
+						}
 						else	
 							elements[i].removeAttribute("readonly");
 					}
 				}
 			}
+			
 
+			<?php    
+				if(isset($_POST['SubmitButton']))
+				{
+				echo("ERERER");
+
+				$sql = "INSERT INTO Persons (FirstName) VALUES ('$input')";
+										$sql = "INSERT INTO Transaction (Description, Comment, TransactionDate, PaymentDate, ResponsibleParty, AssociatedParty, Amount) VALUES ('$_POST['Description']',
+										'$_POST['Comment']',
+										'$_POST['TransactionDate']',
+										'$_POST['PaymentDate']',
+										'$_POST['ResponsibleParty']',
+										'$_POST['AssociatedParty']',
+										'$_POST['Amount']');";
+					mysql_query($sql) or die(mysql_error());
+				} 
+				//TODO else	
+			?> 
 		</script>	
-	</head>
-
+	
 	<body>
 		<div id="main">
 		
@@ -123,7 +149,7 @@
 
 					<?php
 					
-						// Connet to database
+						// Connect to database
 						$connection =mysql_connect("localhost","test","test") or die("Could not connect");	
 						mysql_select_db("test") or die("Unable to select database");
 
@@ -135,106 +161,127 @@
 					<table class = "formatted">
 						<!-- action="toMe.php" -->
 
-						<form name="transactionForm"  onsubmit="return validateForm(this)" action="toMe.php" method="post">
-							<tr>
-								<td class = "transactionTitle">
-									Transaction Name*:
-								</td>
-								<td>
-									<input type="text" class="data" name="TransactionName" value="<?=$row['TransactionName'];?>" readonly="readonly">
-								</td>
-								<td class = "transactionTitle">
-									Status*:
-								</td>
-								<td>
-									<select>
-										<option value="pending">Pending</option>
-										<option value="complete">Complete</option>
-										<!-- selected="selected" -->
-									</select>
-								</td>
-							</tr>
-							<tr>
-								<td class = "transactionTitle">
-									Transaction Date*:
-								</td>
-								<td>
-									<input type="datetime" class="data" name="Date_trans" size="8" value="" readonly="readonly">
-								</td>
-								<td class = "transactionTitle">
-									Amount*:
-								</td>
-								<td>
-									<input type="text" class="data" name="Amount" size="8"  value="<?=$row['Amount'];?>" readonly="readonly">
-								</td>
-							</tr>
-							<tr>
-								<td class = "transactionTitle">
-									Date of receipt/payment*:
-								</td>
-								<td>
-									<input type="datetime" class="data" name="Date_payRec" size="8" readonly="readonly">
-								</td>
-								<td class = "transactionTitle">
-									Type*:
-								</td>
-								<td>
+						<form name="transactionForm"   action="" method="post">
+						<tr>
+							<td  colspan = "2" class = "transactionTitle">
+								Transaction Description
+							</td>
+							<td></td>
+							<td>
+								<select>
+									<option value="" selected="selected"></option>
+									<option value="pending">Pending</option>
+									<option value="complete">Complete</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="4" class = "spaceBelow">
+								<textarea class="data" name="Description" readonly="readonly"></textarea>
+							</td>
 							
-									<input type="radio" class="data" name="Type" value="in">Inflow <br>
-									<input type="radio" class="data" name="Type" value="out">Outflow<br>
-								</td>
-							</tr>
-							<tr>
-								<td class = "transactionTitle">
-									Responsible*:
-								</td>
-								<td>
-									<input type="text" class="data" name="Resp" size="12" readonly="readonly">
-								</td>
-							</tr>
-							<tr>
-								<td class = "transactionTitle spaceBelow">
-									Associated person:
-								</td>
-								<td>
-									<input type="text" class="data" name="assoc" size="12" readonly="readonly">
-								</td>
-							</tr>
-							<tr>
-								<td class = "transactionTitle">
-									Description*:
-								</td>
-								<td>
-									<textarea cols="20" class="data" name="Description" readonly="readonly"><?=$row['Description'];?></textarea>
-								</td>
-							</tr>
-							<tr>
-							<tr>
-								<td class = "transactionTitle spaceBelow">
-									Comment:
-								</td>
-								<td>
-									<textarea cols="20" class="data" name="Comment" readonly="readonly"></textarea>
-								</td>
-							</tr>
-					</table>
-<!--						<button type="Reset">Clear</button>
- -->						<input type="submit" value="Submit" class="button">
+						</tr>
+						<tr>
+							<td class = "transactionTitle">
+								Transaction Date*:
+							</td>
+							<td>
+								<input type="datetime" class="data" name="TransactionDate" size="12" value="" readonly="readonly">
+							</td>
+							<td class = "transactionTitle">
+								Amount*:
+							</td>
+							<td>
+								<input type="text" class="data" name="Amount" size="8"  value="<?=$row['Amount'];?>" readonly="readonly">
+							</td>
+						</tr>
+						<tr>
+							<td class = "transactionTitle">
+								Date of receipt/payment*:
+							</td>
+							<td>
+								<input type="datetime" class="data" name="PaymentDate" size="12" readonly="readonly">
+							</td>
+							<td class = "transactionTitle">
+								Type*:
+							</td>
+							<td>
+								<input type="radio" class="data" name="Type" value="in">Inflow <br>
+								<input type="radio" class="data" name="Type" value="out">Outflow<br>
+							</td>
+						</tr>
+						<tr>
+							<td class = "transactionTitle">
+								Responsible*:
+							</td>
+							<td>
+								<input type="text" class="data" name="ResponsibleParty" size="12" readonly="readonly">
+							</td>
+						</tr>
+						<tr>
+							<td class = "transactionTitle spaceBelow">
+								Associated person:
+							</td>
+							<td>
+								<input type="text" class="data" name="AssociatedParty" size="12" readonly="readonly">
+							</td>
+						</tr>
+						<tr>
+						<tr>
+							<td class = "transactionTitle">
+								Comment:
+							</td>
+						</tr>
+						<tr>
+							<td colspan = "2">
+								<textarea cols="20" class="data" name="Comment" readonly="readonly"><?=$row['Description'];?></textarea>
+							</td>
+						</tr>
+				</table>
+						<button type="Reset">Clear</button>
+						<input type="submit" name="SubmitButton" value="Submit" class="button">
+
+					</form>
+						<button onclick="setReadonly('data',false)">Edit</button>
+						<button onclick="setReadonly('data',true)">Cancel</button>
+					
 							
-						</form>
-							<button onclick="setReadonly('data',false)">Edit</button>
-							<button onclick="setReadonly('data',true)">Cancel</button>
-				</div>
 
+						
+						
+						
+				     		<?php
+			$sql="SELECT * FROM Persons";
+			$result =   mysql_query($sql) or die(mysql_error());
+			echo "<table border='1'>";
 
+			while($row = mysql_fetch_assoc($result))
+			{
+				echo "<tr>";
+				foreach($row as $cname => $cvalue)
+				{
+					echo "<td>" ;
+					print "$cname:  $cvalue\t";
+					echo "</td>";
+				}
+				echo"</tr>";
+				}
+			echo "</table>"; 
+			return mysql_num_rows($result);
+
+		
+		
+		?>
+		</div>
+  	
 				<!-- end content!-->
-				
-	   
-			</div><!-- end box -->
-			
-			<div id="sidebar">
-			
-				<h2>Random side bar info</h2>
+                
+
+            </div><!-- end box -->
+            
+            <div id="sidebar">
+            
+                <h2>Random side bar info</h2>
 				<p>	Stuff
 				</p>
 				<ul>
@@ -242,9 +289,17 @@
 					<li>Point 2</li>
 				</ul>
 
-			</div><!-- end sidebar -->
-			
-		</div><!-- end main -->   
+				
+                
+            </div><!-- end sidebar -->
+            
+            
+        </div><!-- end main -->
+        
 
-	</body>
+       
+    
+
+    
+</body>
 </html>
