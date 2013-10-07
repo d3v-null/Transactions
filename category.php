b4870508439d45a1500d0e341d392fe7cdd286a1
methyleneprince <methyleneprince@gmail.com>
2013-10-07 23:03:45 +0800

    Category working

--- category.php
+++ category.php
@@ -22,85 +22,70 @@
 
     <head>
         <title>Category Details</title>
-        
         <link rel="stylesheet" type="text/css" href="/css/style2.css">
         <link rel="stylesheet" type="text/css" href="/css/styling.css">
-        
-		<script>
-			// Validation function ------ start
-			// http://www.w3schools.com/js/js_form_validation.asp
-			function validateForm()
-			{
-				var name = document.forms["categoryForm"]["Name"].value;
-				
-				if (name == null || name == "")
-				{
-					alert("First Name must be filled out");
-					return false;
-				}
-			}			
-			// validate function  ----- end			
-        </script>
     </head>
 	
     <body id="main">
-        
-		<?php
+        <?php
+            //Has the page been given a category ID?
             $id=(key_exists('id', $_GET)) ? $_GET["id"] : die("No category specified");
             
-            $sql = "SELECT * FROM category WHERE ID=" . $id . "";
-            $result = mysql_query($sql) or die(mysql_error());
-            $row = mysql_fetch_assoc($result);	
+            //Does the ID Correspond to a valid category?
+            $sql = "SELECT Name, Description FROM category WHERE ID=" . $id . "";
+            $result = mysql_query($sql) or die("Category.ID not specified correctly: ".mysql_error());
+            if(!$result) die("No categories in database match given ID: ".mysql_error());
+            $row = mysql_fetch_array($result);
             
             $name = $row['Name'];
-            $desc = $row['Description'];    
+            $desc = $row['Description'];   
             
             if(!empty($_POST)){
-                //Override 
-                if( key_exists('name', $_POST) ) {
-                    $name = $_POST['name'];
-                }
-                if( key_exists('desc', $_POST) ) {
-                    $desc = $_POST['desc'];
-                }
-                if($name = ""){
-                    echo "<script>alert('Name cannot be empty')</script>";
+                if(!key_exists('name', $_POST)) {
+                    echo "<script>alert('No name specified in $_POST')</script>";
+                } else if(!key_exists('desc', $_POST)){
+                    echo "<script>alert('No desc specified in $_POST')</script>";
+                } else if($_POST['name'] == ""){
+                    echo "<script>alert('Name must not be empty')</script>";
                 } else If(!$user->isTreasurer()){
-                    echo "<script>alert('Cannot update category without Treasurer privileges')</script>";
+                    echo "<script>alert('You must have treasurer privileges to modify a category')</script>";
                 } else {
-                    $sql = "UPDATE category SET Name = ".$name.", Description = ".$desc." WHERE category.ID = $id";
-                    mysql_query($sql) or die(mysql_error());
-                    echo "<script>alert('Successfully updated category')</script>";
+                    $name = $_POST['name'];
+                    $desc = $_POST['desc'];
+                    mysql_query("UPDATE category SET Name='".$name."', Description='".$desc."' ".
+                                "WHERE category.ID=".$id) or die(mysql_error());
+                    //echo "<script>alert('Successfully updated category')</script>";
                 }
             }
+            //echo "id: ".$id." name: ".$name." desc: ".$desc." row: ".$row;
         ?>
 		
 		<div id="box">
 			<h1>Category Details</h1>
-			<form name="categoryForm"  id="content" action="category.php?id=<?php $id?>" method="post">
-            <input type="hidden" name="id" value=>
-            
-            <b>* This field is compulsory</b>
-            <table class = "formatted">						
-                <tr class = "spaceBelow">
-                    <td>Name*:</td>
-                    <td><input type="text" name="name" class="data" value="<?php $row['Name'] ?>"></td>							
-                </tr>
-                <tr class = "spaceBelow">
-                    <td>Description:</td>
-                    <td><textarea name="desc" class="data"><?php $row['Description'] ?></textarea></td>							
-                </tr>    
-            </table>
-            
-            <input type="submit" value="Save">
-            <form action="categoryDelete.php" method="get">
-                <input type="hidden" name="id" value="<?php echo $id ?>">
-                <input type="submit" value="Delete">
-            </form>
-        </form>     
-        
+			<form action="category.php?id=<?php echo $id; ?>" method="post" id="content" >
+                <input type="hidden" name="id" value=>
+                
+                <b>* This field is compulsory</b>
+                <table class = "formatted">						
+                    <tr class = "spaceBelow">
+                        <td>Name*:</td>
+                        <td><input type="text" name="name" class="data" value="<?php echo $name ?>"></td>							
+                    </tr>
+                    <tr class = "spaceBelow">
+                        <td>Description:</td>
+                        <td><textarea name="desc" class="data"><?php echo $desc ?></textarea></td>							
+                    </tr>    
+                </table>
+                
+                <input type="submit" value="Save">
+                <form action="categoryDelete.php" method="get">
+                    <input type="hidden" name="id" value="<?php echo $id ?>">
+                    <input type="submit" value="Delete">
+                </form>
+            </form>     
+        </div><!-- end box-->
         <div id="sidebar">      
-				<?php include_once("sidebar.php");?>                 
+				<?php //include_once("sidebar.php");?>                 
         </div><!-- end sidebar -->      
 	</body>
 </html>
