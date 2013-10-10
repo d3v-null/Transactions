b3aa3bd50398f99cb5dc9ce02b3e639152905a01
tiagogarcia <21260915@student.uwa.edu.au>
2013-10-10 18:47:43 +0800

    Formatting Code - Sidebar

--- sidebar.php
+++ sidebar.php
@@ -26,36 +26,36 @@
       <div class="panel-group" id="accordion">
         <?php
         // Select everything from Category
-            $cats = mysql_query("SELECT * FROM Category");
+            $sql = mysql_query("SELECT * FROM Category");
             // For each row of Category
-            while ($row = mysql_fetch_array($cats)) {
+            while ($row = mysql_fetch_array($sql)) {
                 // Save Category ID
                 $catID = $row['ID'];
 
-              echo "<div class='panel panel-default'>";
-              echo "<div class='panel-heading'>";
-              echo "<h4 class='panel-title'>";
-              echo "<a class='accordion-toggle' data-toggle='collapse' data-parent='#accordion' href='#expanded".$row['ID']."''>
-                <span>".$row['Name']."</span></a>";
-              echo "<a href='category.php?id=".$row['ID']."'>
-                      <img src='images/pencil.png' border='0' align='right' />
-                    </a>
-                  </h4>
-                </div>";
-                $subCats = mysql_query("SELECT * FROM SubCategory WHERE SubCategory.CategoryID=".$catID." 
-                                        ORDER BY Name ASC");
-                echo "<div id='expanded".$row['ID']."'' class='panel-collapse collapse'>
-                  <div class='panel-body'>";
-                    while ($subRow = mysql_fetch_array($subCats)) 
-                    {
-                      echo "<li><a href='subcategory.php?id=".$subRow['ID']."'>
-                      <span>".$subRow['Name']."</span></a></li>";
-                    }
-                    echo "<li class='last'><a href='subcategoryNew.php?ID=".$catID."'>
-                      <span>Add New Subcategory</span></a></li>";
-                  echo "</div>";
-                echo"</div>";
-              
+				echo "<div class='panel panel-default'>";
+				echo "<div class='panel-heading'>";
+				echo "<h4 class='panel-title'>";
+				echo "<a class='accordion-toggle' data-toggle='collapse' data-parent='#accordion' href='#expanded".$row['ID']."''>
+					<span>".$row['Name']."</span></a>";
+				echo "<a href='category.php?id=".$row['ID']."'>
+					<img src='images/pencil.png' border='0' align='right' />
+					</a>
+				  </h4>
+				</div>";
+				$subCats = mysql_query("SELECT * FROM SubCategory WHERE SubCategory.CategoryID=".$catID." 
+										ORDER BY Name ASC");
+				echo "<div id='expanded".$row['ID']."'' class='panel-collapse collapse'>
+					<div class='panel-body'>";
+					while ($subRow = mysql_fetch_array($subCats)) 
+					{
+						echo "<li><a href='subcategory.php?id=".$subRow['ID']."'>
+						<span>".$subRow['Name']."</span></a></li>";
+					}
+					echo "<li class='last'><a href='subcategoryNew.php?ID=".$catID."'>
+					  <span>Add New Subcategory</span></a></li>";
+				  echo "</div>";
+				echo"</div>";
+			  
             }
       ?>
     </div>
