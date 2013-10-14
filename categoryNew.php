8fb5ef0afdf0a72c7714287962e316ac38a5dc01
tiagogarcia <21260915@student.uwa.edu.au>
2013-10-14 10:41:51 +0800

    Only treasurers can add new category

--- categoryNew.php
+++ categoryNew.php
@@ -11,7 +11,11 @@
     mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error());
     mysql_select_db(DB_NAME) or die(mysql_error());
     
-    $sql="INSERT INTO Category (Name, Description) VALUES ('New Category','')";
-    mysql_query($sql) or die("Category cannot be created: ".mysql_error());
-    redirect("category.php?id=".mysql_insert_id());
+	if (!$user->isTreasurer()){
+        echo "<script>alert('You must have treasurer privileges to create a new category')</script>";
+    } else {
+		$sql="INSERT INTO Category (Name, Description) VALUES ('New Category','')";
+		mysql_query($sql) or die("Category cannot be created: ".mysql_error());
+		redirect("category.php?id=".mysql_insert_id());
+	}
 ?>    
