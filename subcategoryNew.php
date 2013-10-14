f79fbab3dd46b95877618382ad0c1db84e71fec1
xiaofan2406 <xiaofan2406@gmail.com>
2013-10-14 16:10:50 +0800

    more linking error fix

--- subcategoryNew.php
+++ subcategoryNew.php
@@ -1,30 +1,30 @@
 <?php
     require_once 'includes/config.php';
     require_once 'includes/transaction_setup.php';
-	
+
 	$user = new User();
-	
+
 	if (!$user->isTreasurer()){
 		echo "<script>alert('You must have treasurer privileges to create a new category. You are going to be redirected to the main page')</script>";
 		echo "<meta http-equiv='Refresh' content='0; URL=search.php'>";
-    } else {		
+    } else {
 		// Inserting new category
 		if (key_exists("subCatName", $_POST) && key_exists("subCatDesc", $_POST) && key_exists("subCatID", $_POST))
-		{			
+		{
 			$catName = $_POST['subCatName'];
 			$description = $_POST['subCatDesc'];
 			$catID = $_POST['subCatID'];
-			
+
 			// Check for duplicates
 			$sql = "SELECT Name FROM subcategory WHERE Name LIKE '$catName'";
 			$row = mysql_query($sql);
 			$exists = mysql_fetch_array($row);
-			
-			if(!$exists){				
+
+			if(!$exists){
 				// Insert values
 				$slq="INSERT INTO subcategory (CategoryID, Name, Description) VALUES ('$catID','$catName','$description')";
 				mysql_query($slq);
-				
+
 				echo "<script>alert('Subcategory ". $catName ." was successfully inserted.')</script>";
 			} else {
 				echo "<script>alert('ERROR: Impossible insert subcategory named as ". $catName ." because it already exists in current database. Please specify other name.')</script>";
@@ -38,10 +38,10 @@
 <html>
 	<head>
 		<title>Create New Sub Category</title>
-		
+
 		<style type="text/css" media="screen">
-			@import url("/css/style2.css");
-			@import url("/css/styling.css");
+			@import url("css/style2.css");
+			@import url("css/styling.css");
 		</style>
 
 		<script>
@@ -50,25 +50,25 @@
 			function validateForm()
 			{
 				var error = document.forms["SubCategoryForm"]["subCatName"].value;
-				
+
 				if (error == null || error == "")
 				{
 					alert("First Name must be filled out");
 					return false;
 				}
-			}		
+			}
 		</script>
 	</head>
 
 	<body>
 		<div id="main">
-		
+
 			<div id="box">
 				<h1>Create New Sub Category</h1>
-					
+
 				<div id="content">
 
-					<table class = "formatted">			
+					<table class = "formatted">
 
 						<form name="SubCategoryForm" action="subcategoryNew.php?ID=<?php echo $_GET["ID"]; ?>" onsubmit="return validateForm()" method="post">
 							<tr>
@@ -96,22 +96,22 @@
 								</td>
 							</tr><tr>* This field is compulsory</tr>
 					</table>
-					<br>	
-						<button input type="submit">Save Sub Category</button>				
+					<br>
+						<button input type="submit">Save Sub Category</button>
 						</form>
 
 				</div>
 				<!-- end content!-->
 
 			</div><!-- end box -->
-			
+
 			<div id="sidebar">
-		
+
 				<?php include_once("sidebar.php");?>
-           
+
        		</div><!-- end sidebar -->
-			
-		</div><!-- end main -->   
+
+		</div><!-- end main -->
 
 	</body>
 </html>
